7cecf4f22104688164145fa7182a5e7a99a25fb6
jefdrilon <jeffrydrilon@yahoo.com>
2025-05-12 22:06:04 +0800

    administrator modal

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "builds")
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.precompile += %w( bootstrap-icons.woff2 bootstrap-icons.woff )
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
